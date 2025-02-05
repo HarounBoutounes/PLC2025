e1145c7b4cecfa141dbbea73088e67c4af89dca2
Haroun Boutounes <230370429@aston.ac.uk>
2025-02-05 06:53:50 +0000

    Test3

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -4,8 +4,8 @@ main =
     do
     print (myFunc1 5)
     print (MyFunc2 10)
-    print (3rdFunc)
+    print (thirdFuncFunc)
 
 myFunc1 x = x*10
 MyFunc2 x = x*2
-3rdFunc = "Hello, this is 3rd Func"
+thirdFunc = "Hello, this is 3rd Func"
